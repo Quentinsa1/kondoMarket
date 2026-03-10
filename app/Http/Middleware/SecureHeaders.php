cf7ecab32f359a167ad3f34a915963d2ddfb2d58
Quentin <quentinsacla@gmail.com>
2026-03-10 10:55:20 +0100

    Improve security headers

--- app/Http/Middleware/SecureHeaders.php
+++ app/Http/Middleware/SecureHeaders.php
@@ -10,17 +10,23 @@ class SecureHeaders
 {
     /**
      * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-   public function handle($request, Closure $next)
-{
-    $response = $next($request);
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $next($request);
 
-    return $response->header('X-Frame-Options', 'SAMEORIGIN')
-        ->header('X-XSS-Protection', '1; mode=block')
-        ->header('X-Content-Type-Options', 'nosniff')
-        ->header('Referrer-Policy', 'no-referrer-when-downgrade')
-        ->header('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
-}
+        return $response
+            ->header('X-Frame-Options', 'SAMEORIGIN')
+            ->header('X-Content-Type-Options', 'nosniff')
+            ->header('X-XSS-Protection', '1; mode=block')
+            ->header('Referrer-Policy', 'strict-origin-when-cross-origin')
+            ->header('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload')
+            ->header('Cross-Origin-Resource-Policy', 'same-origin')
+            ->header('Cross-Origin-Opener-Policy', 'same-origin')
+            ->header('Cross-Origin-Embedder-Policy', 'require-corp')
+            ->header(
+                'Content-Security-Policy',
+                "default-src 'self'; img-src 'self' https: data:; script-src 'self' https:; style-src 'self' https: 'unsafe-inline'; object-src 'none'; frame-ancestors 'self'; base-uri 'self'; form-action 'self';"
+            );
+    }
 }
